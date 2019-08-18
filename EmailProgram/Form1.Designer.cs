aa48cb98d15518d7831b4e72af86fb2db0c514ef
Orxan Musayev <orxanmusayev1029@gmail.com>
2019-08-18 11:15:08 +0400

    Send Email with C#

---
+++ EmailProgram/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace EmailProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTo = new System.Windows.Forms.Label();
+            this.txtto = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.lblSubject = new System.Windows.Forms.Label();
+            this.txtBody = new System.Windows.Forms.TextBox();
+            this.lblBody = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTo
+            // 
+            this.lblTo.AutoSize = true;
+            this.lblTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTo.Location = new System.Drawing.Point(65, 88);
+            this.lblTo.Name = "lblTo";
+            this.lblTo.Size = new System.Drawing.Size(45, 25);
+            this.lblTo.TabIndex = 0;
+            this.lblTo.Text = "To:";
+            // 
+            // txtto
+            // 
+            this.txtto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtto.Location = new System.Drawing.Point(137, 88);
+            this.txtto.Name = "txtto";
+            this.txtto.Size = new System.Drawing.Size(260, 26);
+            this.txtto.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SkyBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(137, 387);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSubject.Location = new System.Drawing.Point(137, 140);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(260, 26);
+            this.txtSubject.TabIndex = 4;
+            // 
+            // lblSubject
+            // 
+            this.lblSubject.AutoSize = true;
+            this.lblSubject.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubject.Location = new System.Drawing.Point(18, 139);
+            this.lblSubject.Name = "lblSubject";
+            this.lblSubject.Size = new System.Drawing.Size(92, 25);
+            this.lblSubject.TabIndex = 3;
+            this.lblSubject.Text = "Subject:";
+            // 
+            // txtBody
+            // 
+            this.txtBody.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBody.Location = new System.Drawing.Point(137, 190);
+            this.txtBody.Multiline = true;
+            this.txtBody.Name = "txtBody";
+            this.txtBody.Size = new System.Drawing.Size(260, 175);
+            this.txtBody.TabIndex = 6;
+            // 
+            // lblBody
+            // 
+            this.lblBody.AutoSize = true;
+            this.lblBody.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBody.Location = new System.Drawing.Point(42, 189);
+            this.lblBody.Name = "lblBody";
+            this.lblBody.Size = new System.Drawing.Size(68, 25);
+            this.lblBody.TabIndex = 5;
+            this.lblBody.Text = "Body:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(426, 442);
+            this.Controls.Add(this.txtBody);
+            this.Controls.Add(this.lblBody);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.lblSubject);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtto);
+            this.Controls.Add(this.lblTo);
+            this.Name = "Form1";
+            this.Text = "EmailSender";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTo;
+        private System.Windows.Forms.TextBox txtto;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.Label lblSubject;
+        private System.Windows.Forms.TextBox txtBody;
+        private System.Windows.Forms.Label lblBody;
+    }
+}
+
---
+++ EmailProgram/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+
+namespace EmailProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MailMessage mailMessage = new MailMessage("orxanmusayev2910@gmail.com", txtto.Text, txtSubject.Text, txtBody.Text);
+                SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+                smtpClient.Credentials = new System.Net.NetworkCredential("orxanmusayev2910@gmail.com", "Orxan2910");
+                smtpClient.EnableSsl = true;
+                smtpClient.Send(mailMessage);
+                MessageBox.Show("Mail göndərildi");
+                //mailMessage.IsBodyHtml = true;
+                //mailMessage.Priority = MailPriority.Normal;
+                //mailMessage.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+                //smtpClient.UseDefaultCredentials = false;
+                //smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Mail göndərilmədi... " +exc.Message);           
+            }
+        }
+    }
+}
